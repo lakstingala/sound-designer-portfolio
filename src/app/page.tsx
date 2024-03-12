5a2d6d733d2883c369e152e063cc9655a8e289e8
Lina <lina.survilaite@gmail.com>
2024-03-12 15:02:16 +0100

    Lint fix.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,17 @@ import ModalVideo from "@/components/videoModal"
 import { useState } from "react"
 
 export default function Home() {
-    const [embedId, setEmbedId] = useState("")
-    return <div className="flex flex-col md:flex-row justify-between">
-        {["1", "2", "2"].map(x => {
-            return <ImageCell thumb={"/moana.png"} thumbAlt={""} thumbWidth={1000} thumbHeight={1000} onClick={function (): void {
-                setEmbedId("Fun")
-            }} />
-        })}
-        <ModalVideo embedId={embedId} onClose={function (): void {
-            setEmbedId("")
-        } } />
-    </div>
+  const [embedId, setEmbedId] = useState("")
+  return <div className="flex flex-col md:flex-row justify-between">
+    {["1", "2", "3"].map(x => {
+      return <div key={x}>
+        <ImageCell thumb={"/moana.png"} thumbAlt={""} thumbWidth={1000} thumbHeight={1000} onClick={() => {
+          setEmbedId("Fun")
+        }} />
+      </div>
+    })}
+    <ModalVideo embedId={embedId} onClose={function (): void {
+      setEmbedId("")
+    }} />
+  </div >
 }
