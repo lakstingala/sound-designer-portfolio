96d22430a40ce7c2cfbb37240063a72ca4631295
Lina <121810451+lakstingala@users.noreply.github.com>
2024-03-21 22:39:52 +0800

    Update topMenu.tsx

--- src/components/topMenu.tsx
+++ src/components/topMenu.tsx
@@ -6,7 +6,7 @@ const TopMenu = () => {
     return (
         <>
           <div className="w-full flex flex-row justify-between border border-red p-[12px] bg-[#DFDED0]">
-            <h1 className="text-[24px] text-[#1C221F]">Karolis Balčiu5s</h1>
+            <h1 className="text-[24px] text-[#1C221F]">Karolis Balčius</h1>
             <div className="flex flex-row space-x-[20px]">
                 <Image className="shadow-2xl transition-shadow duration-300 ease-in-out" src={"/icon.svg"} width={28} height={28} alt={"icon"} />
                 <Image className="shadow-2xl transition-shadow duration-300 ease-in-out" src={"/icon_2.svg"} width={28} height={28} alt={"icon_2"} />
