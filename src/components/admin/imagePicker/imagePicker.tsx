7ad3263dc2f5888343e083005a25de2a72fd564f
Lina <lina.survilaite@gmail.com>
2024-04-28 02:38:43 +0300

    Fix image edit bug

--- src/components/admin/imagePicker/imagePicker.tsx
+++ src/components/admin/imagePicker/imagePicker.tsx
@@ -12,7 +12,7 @@ export interface ImagePickerProps {
 }
 
 export const ImagePicker = (p: ImagePickerProps) => {
-    const [isEditing, setIsEditing] = React.useState(false);
+    const [isEditing, setIsEditing] = React.useState(true);
     const [progress, setProgress] = React.useState(0);
     const [isUploading, SetIsUploading] = React.useState(false);
     const [image, setImage] = React.useState<any>(null);
@@ -76,13 +76,23 @@ export const ImagePicker = (p: ImagePickerProps) => {
                 ?
                 <progress value={progress} max="100" className="progress" />
                 :
-                <>
+                <div className='flex items-center space-between space-x-8'>
+                    {p.value && <div
+                        style={{
+                            backgroundImage: `url(${p.value})`,
+                            backgroundSize: 'contain',
+                            backgroundPosition: 'center',
+                            backgroundRepeat: 'no-repeat',
+                            margin: 5
+                        }}
+                        className="flex text-indigo-500 text-white items-center bg-white p-1 round-md flex-none w-16 h-16 md:w-16 md:h-12 m-2">
+                    </div>}
                     <div className="btn">
                         <span>File</span>
                         <input type="file" onChange={handleChange} />
                     </div>
                     <button onClick={() => setIsEditing(false)}>Cancel</button>
-                </>
+                </div>
             }
         </div>
     )
