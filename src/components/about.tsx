f756df89dcdb59a19801d491d631e2f5f0e70212
Lina <lina.survilaite@gmail.com>
2024-04-28 02:48:10 +0300

    Fix fint color in about.

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -13,8 +13,8 @@ const About = () => {
         <div className="relative w-full flex flex-col md:flex-row h-[840px] md:h-[600px] p-[12px] bg-[#1C221F] md:justify-center">
             <Image className="absolute bottom-[0] right-[0] shadow-2xl transition-shadow duration-300 ease-in-out md:h-[600px] md:w-[858px]" src={"/karolis.jpg"} width={1340} height={941} alt={"icon"} />
             <div className='w-full max-w-[1152px] z-10'>
-                <h1 className={"text-[32px] pb-[20px] " + popins.className}>I’m Karolis.</h1>
-                <div className={'w-full max-w-[490px] pr-[40px] text-[20px] space-y-[10px] ' + monserat.className}>
+                <h1 className={"text-[32px] pb-[20px] text-[#DFDED0] " + popins.className}>I’m Karolis.</h1>
+                <div className={'w-full max-w-[490px] pr-[40px] text-[20px] space-y-[10px] text-[#DFDED0] ' + monserat.className}>
                     <p>Hi you beautiful human being! </p>
                     <p>I can make music and create the sound design for your brand, commercial, cgi, video game, or produce you entirely as an artist from A to Z. </p>
                     <p>I’ve been working in the field for over 20 years now and worked with the likes of Redbull, Atari, Swedbank, NBC and blah blah blah…</p>
